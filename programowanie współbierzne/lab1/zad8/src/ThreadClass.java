20eb2f34a8ce37c94d87b96b9551125d707e3422
Popsonikk <popson2001@onet.pl>
2024-03-05 15:31:29 +0100

    javaThread1.8

---
+++ programowanie współbierzne/lab1/zad8/src/ThreadClass.java
@@ -0,0 +1,25 @@
+public class ThreadClass extends Thread{
+    private Licznik licznik;
+    private boolean flag;
+    private int zmiana;
+
+    public ThreadClass(Licznik licznik, boolean flag, int zmiana) {
+        this.licznik = licznik;
+        this.flag = flag;
+        this.zmiana = zmiana;
+    }
+    @Override
+    public void run()
+    {
+        if(flag)
+        {
+            for(int i=0;i<zmiana;i++)
+                licznik.inc();
+        }
+        else
+        {
+            for(int i=0;i<zmiana;i++)
+                licznik.dec();
+        }
+    }
+}
