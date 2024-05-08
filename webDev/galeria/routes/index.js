db76c897f209ad232ea29e9a310129dd5d9a5dfb
Popsonikk <popson2001@onet.pl>
2024-05-08 10:27:24 +0200

    galeria zad8

--- webDev/galeria/routes/index.js
+++ webDev/galeria/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
+const Gallery = require('../models/gallery');
+const Image = require('../models/image');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Galeria' });
+router.get('/', async function(req, res, next) {
+  try{
+    const usersCount = await User.countDocuments();
+    const galleriesCount = await Gallery.countDocuments();
+    const imagesCount = await Image.countDocuments();
+    res.render('index', { title: 'Galeria' ,user:usersCount,gallery:galleriesCount,image:imagesCount});
+  }catch (err) {
+    console.error(err);
+    res.status(500).send('Błąd serwera');
+  }
+  
+  
+
+  
 });
 
 module.exports = router;
